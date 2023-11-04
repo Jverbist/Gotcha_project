bfecf0fb14752308de826aa12bff25090b514311
Jverbist <jornt@bist.be>
2023-11-04 17:35:34 +0100

    file structuur

---
+++ main.py
@@ -0,0 +1,41 @@
+# This a gotcha game made by me, it is a simple game with a simple concept.
+
+
+
+
+# Import all the required libraries:
+
+
+
+
+
+
+# app code starts here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# app gets executed here:
+
+
+
+
+# app posts execution code here:
+
+
+
+
+
+
+
