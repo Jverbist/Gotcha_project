c9de5f530fc19484e7794cd94a85101c56de22f9
Jverbist <jornt@bist.be>
2023-11-05 15:55:38 +0100

    Hello World message

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # This a gotcha game made by me, it is a simple game with a simple concept.
+from fastapi import FastAPI
 
 
 
 
 # Import all the required libraries:
-
+app = FastAPI()
 
 
 
@@ -28,7 +29,9 @@
 
 # app gets executed here:
 
-
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
 # app posts execution code here:
