1ef9eccd6d014f52b839d89cf4fe2fe772cc0d84
Jverbist <jornt@bist.be>
2023-11-08 10:06:34 +0100

    Got first html to pop up

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-# This a gotcha game made by me, it is a simple game with a simple concept.
+#This a gotcha game made by me, it is a simple game with a simple concept.
 from fastapi import FastAPI
+from fastapi import Request, Depends
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from db.database import SessionLocal, engine
 
 
 
 
 # Import all the required libraries:
-app = FastAPI()
 
 
 
 
 
 # app code starts here:
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Dependencies:
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # app gets executed here:
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def home_page(request: Request, db: SessionLocal = Depends(get_db)):
+
+    return templates.TemplateResponse("home/index.html", {"request": request})
 
 
 # app posts execution code here:
